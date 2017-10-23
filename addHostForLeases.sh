f83e49d3b9fa857462d340d195a133e75436cadb
Brandon Smitley <bsmitley@andrew.cmu.edu>
2017-10-23 11:16:57 -0400

    Initial commit

---
+++ addHostForLeases.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+####################################################
+#title           :addHostForLeases.sh
+#description     :This script is used to add hosts from the lease file to dhcp.conf
+#author          :Brandon Smitley
+#date            :Last updated October 23, 2017
+#version         :0.2
+####################################################
+#Set your lease file
+leases=dhcpleases
+
+#Set a place to set dhcp leases after they have been parsed 
+dhcpparsed=dhcpparsed
+
+#Set your dhcp.conf
+dhcpconf=dhcp_conf
+
+#Set your dhcp copy
+dhcpcopy=dhcp_copy
+
+#Set lock file variable
+lf=/var/lock/addHostForLeases.lock
+
+
+#if lock file exists exit
+[ -f $lf ] && exit
+
+#creat lock file
+touch $lf
+
+
+#Split the information out of the lease list
+awk 'BEGIN{
+    while( (getline line < "leaselist") > 0){
+        lease[line]
+	}
+    RS="}"
+    FS="\n"
+}
+/lease/{
+
+    for(i=1;i<=NF;i++){
+        gsub(";","",$i)
+        if ($i ~ /lease/) {
+            m=split($i, IP," ")
+            ip=IP[2]
+        }
+        if( $i ~ /hardware/ ){
+            m=split($i, hw," ")
+            mac=hw[3]
+        }
+	#Dont need the hostname
+#	if ( $i ~ /client-hostname/){
+#             m=split($i,ch, " ")
+#             hostname=ch[2]
+#      }
+
+    }
+
+# print ip ","hostname","mac 
+  print ip ","mac
+	
+} ' $leases > $dhcpparsed
+
+
+#Make a copy of DHCP conf to right to the orginal
+cp -p $dhcpconf $dhcpcopy
+
+#See if the mac exists in dhcp.conf
+while IFS=, read ip mac;do
+
+if grep -q  $mac $dhcpcopy; then
+	echo true
+else
+	nscheck=`nslookup  $ip  | grep -i name | awk '{print $4}' | head -1 | sed 's/.$//' | tr '[:upper:]' '[:lower:]'`
+	echo  $mac
+cat <<EOT >> $dhcpcopy 
+
+host $nscheck {
+   option host-name "$nscheck";
+   hardware ethernet $mac;
+   fixed-address $ip;
+}
+
+EOT
+	
+fi
+
+done < $dhcpparsed
+
+#Copy the dhcp_conf_copy to dhcp_conf_copy
+#cp -p $dhcpcopy $dhcpconf
+#rndc reload
+
+#delete the lockfile
+rm $lf
+
---
+++ deleteHost.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+####################################################
+#title           :deleteHost.sh
+#description     :This script is used to delete hosts from dhcp.conf
+#author		 :Brandon Smitley
+#date            :Last updated October 20, 2017
+#version         :0.2    
+####################################################
+#Set lock file variable
+lf=/var/lock/addHostForLeases.lock
+
+#DHCP conf file
+dhcpconf=dhcp_conf_new
+
+#DHCP copy file
+dhcpcopy=dhcp_copy
+
+
+#check to see if the add script is running
+if [ -f $lf ]; then 
+	echo "The Add Host Leases script is still running. Please try again later"
+	exit 0
+fi
+
+#Usage function
+usage() { echo "Usage:
+    deleteHost: $0
+    deleteHost: $0 -f -h <hostname>
+Options:
+  -f	Force
+  -h	Hostname"
+	 exit 1; }
+
+
+#Get Variables of the flags
+while getopts "fh:" o; do
+    case "${o}" in
+        f)
+            f="true"
+            ;;
+        h)
+            h=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+
+#If no flags are provided run the promot flag
+if [ -z "${h}" ] && [ -z "${f}" ]; then	
+	echo $h
+	#Ask for the FQDN and recored it
+	read -p "Enter the fqdn host you want to delete: " hostvar
+
+	#Ask to be 100 percent and record it
+	read -p "You are about to delete $hostvar. Are you sure? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+			#Finds and the recored and removes it and places that output in a new file
+			sed "/$hostvar {/,/^[[:space:]]*$/d" $dhcpconf > $dhcpcopy
+			#Copies to the orginal file
+		        $dhcpcopy > $dhcpconf
+	        	rndc reload
+	fi
+fi
+
+#Check for force flag and make sure a host name is attached
+if  [ "${f}" == "true" ]; then
+	if [ -z "${h}" ]; then
+		echo "Please run the -h flag and provide the FQDN of the host you want to delete."
+	else 	
+		#Finds and the recored and removes it and places that output in a new file
+		sed "/$h {/,/^[[:space:]]*$/d" $dhcpconf > $dhcpcopy
+		#Copies to the orginal file
+		$dhcpcopy > $dhcpconf
+		rndc reload
+		
+	fi
+fi
